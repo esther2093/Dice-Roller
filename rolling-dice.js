9c00ac3de0ac9c7f595572b5778bcb0659e6fb33
Esther2093 <estherlee0420@gmail.com>
2023-07-20 10:46:56 +1000

    finished JS

--- rolling-dice.js
+++ rolling-dice.js
@@ -1,1 +1,19 @@
-let images = ["10-1.jpg", "10-2.jpg", "10-3.jpg", "10-4.jpg", "10-5.jpg", "10-6.jpg", "10-7.jpg", "10-8.jpg", "10-9.jpg", "10-10.jpg"]
+let images1 = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", 
+"images/dice6.png"]
+let images2 = ["images/10-1.jpg", "images/10-2.jpg", "images/10-3.jpg", "images/10-4.jpg", "images/10-5.jpg", 
+"images/10-6.jpg", "images/10-7.jpg", "images/10-8.jpg", "images/10-9.jpg", "images/10-10.jpg"]
+let dice = document.querySelectorAll("img");
+
+function roll1() {
+    let randomimages = Math.floor(Math.random() * images1.length)
+    let randomimageurl = images1[randomimages]
+    document.getElementById('dice6').src = randomimageurl
+    console.log(randomimageurl)
+}
+
+function roll2() {
+    let randomimages = Math.floor(Math.random() * images2.length)
+    let randomimageurl = images2[randomimages]
+    document.getElementById('dice10').src = randomimageurl
+    console.log(randomimageurl)
+}
