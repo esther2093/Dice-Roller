3e6dc427a74126c45b362fa997be97b3847ca619
Esther2093 <estherlee0420@gmail.com>
2023-07-20 11:57:34 +1000

    pretty up the UI

--- rolling-dice.js
+++ rolling-dice.js
@@ -2,7 +2,7 @@ let images1 = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "imag
 "images/dice6.png"]
 let images2 = ["images/10-1.jpg", "images/10-2.jpg", "images/10-3.jpg", "images/10-4.jpg", "images/10-5.jpg", 
 "images/10-6.jpg", "images/10-7.jpg", "images/10-8.jpg", "images/10-9.jpg", "images/10-10.jpg"]
-let dice = document.querySelectorAll("img");
+// let dice = document.querySelectorAll("img");
 
 function roll1() {
     let randomimages = Math.floor(Math.random() * images1.length)
